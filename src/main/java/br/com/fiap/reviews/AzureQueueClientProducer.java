18632cb92a62350c3b878dfacd82b917f6f90119
Arthur Martins <arthur.mezaonik@gmail.com>
2025-12-04 23:47:08 +0000

    Teste 2

--- src/main/java/br/com/fiap/reviews/AzureQueueClientProducer.java
+++ src/main/java/br/com/fiap/reviews/AzureQueueClientProducer.java
@@ -10,17 +10,17 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class AzureQueueClientProducer {
-    @ConfigProperty(name = "azure.storage.queue.connection-string")
-    String connectionString;
-    @ConfigProperty(name = "azure.storage.queue.name")
-    String queueName;
-
-    @Produces
-    @Singleton
-    public QueueClient queueClient() {
-        return new QueueClientBuilder()
-            .connectionString(connectionString)
-            .queueName(queueName)
-            .buildClient();
-    }
+//    @ConfigProperty(name = "azure.storage.queue.connection-string")
+//    String connectionString;
+//    @ConfigProperty(name = "azure.storage.queue.name")
+//    String queueName;
+//
+//    @Produces
+//    @Singleton
+//    public QueueClient queueClient() {
+//        return new QueueClientBuilder()
+//            .connectionString(connectionString)
+//            .queueName(queueName)
+//            .buildClient();
+//    }
 }
