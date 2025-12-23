79445b3913945afa1f5dd7bd99cf84fd07f92f25
Arthur Martins <arthur.mezaonik@gmail.com>
2025-12-23 22:13:32 +0000

    Encode the message tha is being sent to the queue

--- src/main/java/br/com/fiap/reviews/services/NotificationService.java
+++ src/main/java/br/com/fiap/reviews/services/NotificationService.java
@@ -1,6 +1,5 @@
 package br.com.fiap.reviews.services;
 
-import br.com.fiap.reviews.AzureQueueClientProducer;
 import br.com.fiap.reviews.BadReviewPayload;
 import br.com.fiap.reviews.NotificationMessage;
 import br.com.fiap.reviews.NotificationType;
@@ -9,7 +8,9 @@ import com.azure.storage.queue.QueueClient;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.Base64;
 
 @ApplicationScoped
 public class NotificationService {
@@ -33,6 +34,11 @@ public class NotificationService {
             Instant.now()
         );
 
-        queueClient.sendMessage(json.toJson(notificationMessage));
+        String jsonMessage = json.toJson(notificationMessage);
+
+        // Encode as Base64
+        String base64Message = Base64.getEncoder().encodeToString(jsonMessage.getBytes(StandardCharsets.UTF_8));
+
+        queueClient.sendMessage(base64Message);
     }
 }
